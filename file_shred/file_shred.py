f30fc88071cdc1f103318b225baa109d1dd0cf4d
Joseph E.S. Cieslik <unknownmasayoshi@gmail.com>
2014-09-26 14:08:50 -0400

    file_shred
    python file

---
+++ file_shred/file_shred.py
@@ -0,0 +1,28 @@
+import os, sys, platform
+def shredder():
+	print "Shredder v1"
+	print "Please enter a valid file name"
+	#error handling = not yet implemented
+	input = raw_input("File Name: ")
+	confirm = raw_input("Really shred %s? y/n", %(input))
+		if confirm == 'y':
+			pass
+		elif confirm == 'n':
+			sys.exit(0)
+		else:
+			return False
+	shred_obj = open("%s","rb+", %(input))
+	#os.path.getsize(input) 
+	size = os.stat(input)[6] #"%s", %(input)
+	grbg = os.urandom(size)
+	shred_obj.write(grbg)
+	shred_obj.close()
+	overwrite = open("%s","w", %(input))
+	overwrite.close()
+	if platform.system() == "Windows":
+		os.system("cipher /w:C:")
+	else:
+		return False
+	os.remove(input)
+if __name__ == '__main__':
+    shredder()
